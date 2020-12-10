09d4cf33e0b9bb298dfbd716aec78c0f50769fee
Kaique <kaiqueribeiro@alunos.utfpr.edu.br>
2020-12-10 14:53:44 -0300

    melhorias de html css

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ function getLocation(){
 }
 
 document.getElementById('searchBtn').addEventListener('click', () => {
-    let searchTerm = document.getElementById('citynameinput').value 
+    let searchTerm = document.getElementById('citynameinput').value ;
     if (searchTerm)
-        searchWather(searchTerm)
+        searchWather(searchTerm);
 })
 
 function searchLoc(latitude, longitude){
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${myAPI}&units=${unit}`).then(result => {
         return result.json();
     }).then(result => {
-        init(result)
+        init(result);
     })
 }
 
@@ -26,36 +26,36 @@ function searchWather(search){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${myAPI}&units=${unit}`).then(result => {
         return result.json();
     }).then(result => {
-        init(result)
+        init(result);
     })
 }
 function init(resultFromServer){
 
-    let weatherDescription = document.getElementById('weatherDescriptionHeader')
-    let temperature = document.getElementById('temperature')
-    let humidity = document.getElementById('humidity')
-    let windSpeed = document.getElementById('windSpeed')
-    let cityname = document.getElementById('cityname')
-    let minmax = document.getElementById('minmax')
-    let weatherIcon = document.getElementById('weatherIcon')
+    let weatherDescription = document.getElementById('weatherDescriptionHeader');
+    let temperature = document.getElementById('temperature');
+    let humidity = document.getElementById('humidity');
+    let windSpeed = document.getElementById('windSpeed');
+    let cityname = document.getElementById('cityname');
+    let minmax = document.getElementById('minmax');
+    let weatherIcon = document.getElementById('weatherIcon');
 
     if (resultFromServer.cod != 404){
-        weatherIcon.src = 'https://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png'
-        cityname.innerText = resultFromServer.name + ' - ' + resultFromServer.sys.country
-        temperature.innerText = Math.floor(resultFromServer.main.temp) + 'ºC'
-        minmax.innerText = 'Min ' + Math.floor(resultFromServer.main.temp_min)+ 'ºC ' + ' Max ' + Math.floor(resultFromServer.main.temp_max) + 'ºC'
-        humidity.innerText = 'Humidity ' + resultFromServer.main.humidity + '%'
-        windSpeed.innerText = 'Winds at ' + resultFromServer.wind.speed + ' km/h'
-        weatherDescription = resultFromServer.weather[0].description
-        weatherDescriptionHeader.innerText = weatherDescription
+        weatherIcon.src = 'https://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png';
+        cityname.innerText = resultFromServer.name + ' - ' + resultFromServer.sys.country;
+        temperature.innerText = Math.floor(resultFromServer.main.temp) + 'ºC';
+        minmax.innerText = 'Min ' + Math.floor(resultFromServer.main.temp_min)+ 'ºC - ' + ' Max ' + Math.floor(resultFromServer.main.temp_max) + 'ºC';
+        humidity.innerText = 'Humidity ' + resultFromServer.main.humidity + '%';
+        windSpeed.innerText = 'Winds at ' + resultFromServer.wind.speed + ' km/h';
+        weatherDescription = resultFromServer.weather[0].description;
+        weatherDescriptionHeader.innerText = weatherDescription;
         
     }else{
-            cityname.innerText = 'oops! city not found'
-            temperature.innerText = ''
-            humidity.innerText = ''
-            windSpeed.innerText = ''
-            weatherDescriptionHeader.innerText = ''
-            minmax.innerText = ''
-            weatherIcon.src = ''
+            cityname.innerText = 'oops! city not found';
+            temperature.innerText = '';
+            humidity.innerText = '';
+            windSpeed.innerText = '';
+            weatherDescriptionHeader.innerText = '';
+            minmax.innerText = '';
+            weatherIcon.src = '';
     }
 }
