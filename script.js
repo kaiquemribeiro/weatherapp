af3d6e6754a91d32008cc2c586a1d9a1610dceb5
kaiquemribeiro <kaiqueribeiro@alunos.utfpr.edu.br>
2021-01-06 02:25:45 -0300

    -

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
-let myAPI = '32226dc9cba44aa4ee6dd5aa71c0a863';
-const unit = 'metric';
+let myAPI = "32226dc9cba44aa4ee6dd5aa71c0a863";
+const unit = "metric";
 let searchCity;
 
-function getLocation(){
-    navigator.geolocation.getCurrentPosition(function(position) {
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(function (position) {
     searchLoc(position.coords.latitude, position.coords.longitude);
-})
+  });
 }
 
-document.getElementById('searchBtn').addEventListener('click', () => {
-    let searchTerm = document.getElementById('citynameinput').value ;
-    if (searchTerm)
-        searchWather(searchTerm);
-})
+document.getElementById("searchBtn").addEventListener("click", () => {
+  let searchTerm = document.getElementById("citynameinput").value;
+  if (searchTerm) searchWather(searchTerm);
+});
 
-function searchLoc(latitude, longitude){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${myAPI}&units=${unit}`).then(result => {
-        return result.json();
-    }).then(result => {
-        init(result);
+function searchLoc(latitude, longitude) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${myAPI}&units=${unit}`
+  )
+    .then((result) => {
+      return result.json();
     })
+    .then((result) => {
+      init(result);
+    });
 }
 
-function searchWather(searchCity){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myAPI}&units=${unit}`).then(result => {
-        return result.json();
-    }).then(result => {
-        init(result);
+function searchWather(searchCity) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${myAPI}&units=${unit}`
+  )
+    .then((result) => {
+      return result.json();
     })
+    .then((result) => {
+      init(result);
+    });
 }
 
-function init(resultFromServer){
+function init(resultFromServer) {
+  let weatherDescription = document.getElementById("weatherDescriptionHeader");
+  let temperature = document.getElementById("temperature");
+  let humidity = document.getElementById("humidity");
+  let windSpeed = document.getElementById("windSpeed");
+  let cityname = document.getElementById("cityname");
+  let minmax = document.getElementById("minmax");
+  let weatherIcon = document.getElementById("weatherIcon");
+  let flag = document.getElementById("flag");
 
-    let weatherDescription = document.getElementById('weatherDescriptionHeader');
-    let temperature = document.getElementById('temperature');
-    let humidity = document.getElementById('humidity');
-    let windSpeed = document.getElementById('windSpeed');
-    let cityname = document.getElementById('cityname');
-    let minmax = document.getElementById('minmax');
-    let weatherIcon = document.getElementById('weatherIcon');
-    let flag = document.getElementById('flag');
-
-    if (resultFromServer.cod != 404){
-        weatherIcon.src = 'https://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png';
-        cityname.innerText = resultFromServer.name;
-        flag.src = `https://www.countryflags.io/${resultFromServer.sys.country}/flat/64.png`;
-        temperature.innerText = Math.floor(resultFromServer.main.temp) + 'ºC';
-        minmax.innerText = 'Min ' + Math.floor(resultFromServer.main.temp_min)+ 'ºC - ' + ' Max ' + Math.floor(resultFromServer.main.temp_max) + 'ºC';
-        humidity.innerText = 'Humidity ' + resultFromServer.main.humidity + '%';
-        windSpeed.innerText = 'Winds at ' + resultFromServer.wind.speed + ' km/h';
-        weatherDescription = resultFromServer.weather[0].description;
-        weatherDescriptionHeader.innerText = weatherDescription;
-        
-    }else{
-            cityname.innerText = 'oops! city not found';
-            temperature.innerText = '';
-            humidity.innerText = '';
-            windSpeed.innerText = '';
-            weatherDescriptionHeader.innerText = '';
-            minmax.innerText = '';
-            weatherIcon.src = '';
-            flag.src = "transparent_icon.png";
-    }
-
-    console.log(resultFromServer);
+  if (resultFromServer.cod != 404) {
+    weatherIcon.src =
+      "https://openweathermap.org/img/wn/" +
+      resultFromServer.weather[0].icon +
+      ".png";
+    cityname.innerText = resultFromServer.name;
+    flag.src = `https://www.countryflags.io/${resultFromServer.sys.country}/flat/64.png`;
+    temperature.innerText = Math.floor(resultFromServer.main.temp) + "ºC";
+    minmax.innerText =
+      "Min " +
+      Math.floor(resultFromServer.main.temp_min) +
+      "ºC - " +
+      " Max " +
+      Math.floor(resultFromServer.main.temp_max) +
+      "ºC";
+    humidity.innerText = "Humidity " + resultFromServer.main.humidity + "%";
+    windSpeed.innerText = "Winds at " + resultFromServer.wind.speed + " km/h";
+    weatherDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = weatherDescription;
+  } else {
+    cityname.innerText = "oops! city not found";
+    temperature.innerText = "";
+    humidity.innerText = "";
+    windSpeed.innerText = "";
+    weatherDescriptionHeader.innerText = "";
+    minmax.innerText = "";
+    weatherIcon.src = "";
+    flag.src = "transparent_icon.png";
+  }
 }
-
-//teste
